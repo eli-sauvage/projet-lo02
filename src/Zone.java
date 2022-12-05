a400258ebe97fb78071307a0d68118203e56fbbd
Azerty2289 <mercieresteban@orange.fr>
2022-12-05 14:56:22 +0100

    ouo

--- src/Zone.java
+++ src/Zone.java
@@ -1,29 +1,25 @@
+import java.util.*;
 public class Zone {
-    private Etudiant[] combatantsJ1, combatantsJ2;
+    
+    private ArrayList<Etudiant>  combatantsJ1, combatantsJ2;
     private int typeZone;
     //1 = BU, 2 = BDE, 3 = QA, 4 = HI, 5 = HS
 
     public Zone(int type){
         this.typeZone = type;
+        combatantsJ1 = new ArrayList<Etudiant>();
+        combatantsJ2 = new ArrayList<Etudiant>();
     }
-    public Etudiant[] getCombatantsJ1() {
-        return combatantsJ1;
-    }
-
-    public void setCombatantsJ1(Etudiant[] combatantsJ1) {
-        this.combatantsJ1 = combatantsJ1;
-    }
-
-    public Etudiant[] getCombatantsJ2() {
-        return combatantsJ2;
+    public ArrayList<Etudiant> getCombatantsJ(int joueurSelect) {
+        if(joueurSelect==1) return combatantsJ1;
+        if(joueurSelect==2) return combatantsJ2;
+        else return null;
     }
 
     public int getTypeZone() {
         return typeZone;
     }
 
-    public void setCombatantsJ2(Etudiant[] combatantsJ2) {
-        this.combatantsJ2 = combatantsJ2;
-    }
+    
 
 }
