6cbafdaf5a46a1f99a59079cc105e1b1c9cf248a
= <admin@example.com>
2023-01-03 21:41:59 +0100

    test image

--- src/views/CombatsView.java
+++ src/views/CombatsView.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import javax.imageio.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.InputStream;
 
 import controllers.*;
@@ -22,35 +23,30 @@ public class CombatsView {
         frame.setBounds(10, 10, 1500, 800);
         frame.getContentPane().setBackground(bgColor);
         frame.setLayout(null);
-        // JLabel label11 = new JLabel("COMBATS");
-        // label11.setBounds(46, 35, 400, 31);
-        // label11.setFont(new Font("Tahoma", Font.BOLD, 25));
-		// frame.add(label11);
-        // map = new ImageIcon("ressources\\map.png");
-        // frame.add(new JLabel(map));
+        ImageIcon icone = new ImageIcon("ressources/map.png");
+        JLabel image = new JLabel(icone);
+        frame.add(image);
 
-        Container c = frame.getContentPane(); // Gets the content layer
-        JLabel label = new JLabel(); // JLabel Creation
-        BufferedImage image = null;
-        try{
-            System.out.println(getClass().getResource("/src/ressources/map.png"));
-            //image = ImageIO.read();
-        }catch(Exception e){
-            System.out.println(e.toString());
-            System.out.println(e.getMessage());
-            System.out.println(e.getCause());
+        try {
+            File file = new File("ressources/map.png");
+            BufferedImage bufferedImage = ImageIO.read(file);
+
+            ImageIcon imageIcon = new ImageIcon(bufferedImage);
+            JFrame jFrame = new JFrame();
+
+            jFrame.setLayout(new FlowLayout());
+
+            jFrame.setSize(500, 500);
+            JLabel jLabel = new JLabel();
+
+            jLabel.setIcon(imageIcon);
+            jFrame.add(jLabel);
+            jFrame.setVisible(true);
+        } catch (Exception e) {
+            System.out.println(e);
         }
-        label.setIcon(new ImageIcon(image)); // Sets the image to be displayed as an icon
-        Dimension size = label.getPreferredSize(); // Gets the size of the image
-        label.setBounds(50, 30, size.width, size.height); // Sets the location of the image
 
-        c.add(label); // Adds objects to the container
-        frame.setVisible(true); // Exhibit the frame
-
-
-
-
-        frame.setVisible(true);
+        /// frame.setVisible(true);
     }
 
     public void fermer() {
