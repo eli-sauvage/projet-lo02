e5552d076697e74b6c5abc2181769e01f79638ac
elicolh <36312455+elicolh@users.noreply.github.com>
2023-01-10 14:48:02 +0000

    jar ?

--- src/views/TreveView.java
+++ src/views/TreveView.java
@@ -33,15 +33,17 @@ public class TreveView {
 	// private Etudiant etuSelect = new Etudiant(1,1);
 	private Etudiant survivantSelect = null;
 	private Etudiant reservisteSelect = null;
-	private String[] controleZoneString = new String[] {"--", "J1", "J2"};
+	private String[] controleZoneString = new String[] { "--", "J1", "J2" };
 	private JLabel[] lblCreditZone;
 	private JLabel lblCreditTotal;
 	private int numeroJoueur;
 	private int nbCreditEtuSelect;
+	private JLabel lblZoneActuel;
 
 	JLabel lblNbCredit;
+
 	/**
-	 * @param controller le controller associe a la vue
+	 * @param controller   le controller associe a la vue
 	 * @param numeroJoueur le numero du joueur actuel
 	 */
 	public TreveView(TreveController controller, int numeroJoueur) {
@@ -50,12 +52,14 @@ public class TreveView {
 		affTreveView();
 		treveView.setVisible(true);
 	}
+
 	/**
 	 * ferme la fenetre de la treve
 	 */
 	public void fermer() {
 		treveView.setVisible(false);
 	}
+
 	/**
 	 * affiche la vuex de la treve
 	 */
@@ -75,19 +79,19 @@ public class TreveView {
 		reservistes = controller.getReserviste();
 		survivants = controller.getSurvivants();
 
-		//choix de la nouvelle zone du reserviste deploiement
-		//label
+		// choix de la nouvelle zone du reserviste deploiement
+		// label
 		JLabel lblNewLabel = new JLabel("Deploiement reservistes");
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
 		lblNewLabel.setBounds(196, 150, 600, 36);
 		treveView.add(lblNewLabel);
-		//liste du reserviste a selectionner
+		// liste du reserviste a selectionner
 		choixReservistes = new Choice();
 		choixReservistes.setFont(new Font("Tahoma", Font.BOLD, 20));
 		choixReservistes.setBounds(196, 200, 200, 26);
 		choixReservistes.addItemListener(new ItemListener() {
 			public void itemStateChanged(ItemEvent e) {
-				//update de l'affichage du nb de credit
+				// update de l'affichage du nb de credit
 				update();
 			}
 		});
@@ -95,12 +99,12 @@ public class TreveView {
 			choixReservistes.add(etuReserviste.getNom());
 		}
 		treveView.add(choixReservistes);
-		//label
+		// label
 		JLabel lblDestinationReserv = new JLabel("Zone du deploiement");
 		lblDestinationReserv.setFont(new Font("Tahoma", Font.BOLD, 20));
 		lblDestinationReserv.setBounds(196, 250, 300, 26);
 		treveView.add(lblDestinationReserv);
-		//drop down pour la zone de deploiement
+		// drop down pour la zone de deploiement
 		zoneDeploiementReserviste = new Choice();
 		zoneDeploiementReserviste.setFont(new Font("Tahoma", Font.BOLD, 20));
 		zoneDeploiementReserviste.setBounds(196, 300, 200, 26);
@@ -108,18 +112,18 @@ public class TreveView {
 			zoneDeploiementReserviste.addItem(Utils.zoneIndexToString(i));
 		}
 		treveView.add(zoneDeploiementReserviste);
-		//bouton valider reserviste
+		// bouton valider reserviste
 		JButton deployerReserviste = new JButton("Deployer ");
 		deployerReserviste.setBounds(196, 350, 200, 40);
 		deployerReserviste.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if (choixReservistes.getItemCount() > 0 && reservisteSelect != null) {
-					//controller.deployerReserviste();
+					// controller.deployerReserviste();
 					System.out.println(reservisteSelect.getNom());
-					//demande au controller d'effectuer le deploiment
+					// demande au controller d'effectuer le deploiment
 					controller.deployerReserviste(reservisteSelect,
 							zoneDeploiementReserviste.getSelectedIndex());
-					//re affichage de la liste update
+					// re affichage de la liste update
 					choixReservistes.removeAll();
 					reservistes = controller.getReserviste();
 					for (Etudiant etu : reservistes) {
@@ -132,21 +136,21 @@ public class TreveView {
 		});
 		treveView.add(deployerReserviste);
 
-		//---------------------------------------------------------------//
+		// ---------------------------------------------------------------//
 
-		//choix de la zone de deploiement pour les survivants
-		//label
+		// choix de la zone de deploiement pour les survivants
+		// label
 		JLabel lblRedeploiementDesSurvivants = new JLabel("Deploiement survivants");
 		lblRedeploiementDesSurvivants.setFont(new Font("Tahoma", Font.BOLD, 20));
 		lblRedeploiementDesSurvivants.setBounds(896, 150, 600, 36);
 		treveView.add(lblRedeploiementDesSurvivants);
-		//selection du survivant
+		// selection du survivant
 		choixSurvivants = new Choice();
 		choixSurvivants.setFont(new Font("Tahoma", Font.BOLD, 20));
 		choixSurvivants.setBounds(896, 200, 300, 26);
 		choixSurvivants.addItemListener(new ItemListener() {
 			public void itemStateChanged(ItemEvent e) {
-				//update de l'affichage du nb de credit
+				// update de l'affichage du nb de credit
 				update();
 			}
 		});
@@ -154,12 +158,12 @@ public class TreveView {
 			choixSurvivants.add(etuReserviste.getNom());
 		}
 		treveView.add(choixSurvivants);
-		//label
-		JLabel lblZoneActuel = new JLabel("Zone : ");
+		// label
+		lblZoneActuel = new JLabel("Zone : ");
 		lblZoneActuel.setFont(new Font("Tahoma", Font.BOLD, 20));
 		lblZoneActuel.setBounds(1200, 190, 600, 36);
 		treveView.add(lblZoneActuel);
-		//condition si plus de survivant dans la liste
+		// condition si plus de survivant dans la liste
 		if (survivants.size() > 0) {
 			lblZoneActuel.setText("Zone : "
 					+ Utils.zoneIndexToString(survivants.get(0).getZone().getIndiceZone()));
@@ -167,13 +171,13 @@ public class TreveView {
 			lblZoneActuel.setText("Zone : ");
 		}
 
-		//choix de la nouvelle zone d'un survivant
-		//label 
+		// choix de la nouvelle zone d'un survivant
+		// label
 		JLabel lblDestinationSurviv = new JLabel("Zone du deploiement");
 		lblDestinationSurviv.setFont(new Font("Tahoma", Font.BOLD, 20));
 		lblDestinationSurviv.setBounds(896, 250, 300, 26);
 		treveView.add(lblDestinationSurviv);
-		//drop down pour la zone de deploiement
+		// drop down pour la zone de deploiement
 		zoneDeploiementSurvivant = new Choice();
 		zoneDeploiementSurvivant.setFont(new Font("Tahoma", Font.BOLD, 20));
 		zoneDeploiementSurvivant.setBounds(896, 300, 200, 26);
@@ -182,39 +186,39 @@ public class TreveView {
 		}
 		zoneDeploiementSurvivant.addItemListener(new ItemListener() {
 			public void itemStateChanged(ItemEvent e) {
-				//update de l'affichage du nb de credit
+				// update de l'affichage du nb de credit
 				nbCreditEtuSelect = survivants.get(choixSurvivants.getSelectedIndex()).getCredits();
 				update();
 			}
 		});
 		treveView.add(zoneDeploiementSurvivant);
 
-		//bouton valider reserviste
+		// bouton valider reserviste
 		JButton deployerSurvivant = new JButton("Deployer");
 		deployerSurvivant.setBounds(896, 520, 200, 40);
 		deployerSurvivant.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if (choixSurvivants.getItemCount() > 0 && survivantSelect != null) {
-					//deployer survivant sur une zone
+					// deployer survivant sur une zone
 					System.out.println(survivantSelect.getNom());
-					//demande au controller d'effectuer le deploiment
+					// demande au controller d'effectuer le deploiment
 					if (survivantSelect.getZone().getIndiceZone() == zoneDeploiementSurvivant
 							.getSelectedIndex()) {
-								JFrame f = new JFrame();
-								JDialog d;
-								d = new JDialog(f, "Erreur", true);
-								d.setLayout(new FlowLayout());
-								JButton b = new JButton("OK");
-								b.addActionListener(new ActionListener() {
-									public void actionPerformed(ActionEvent e) {
-										d.setVisible(false);
-									}
-								});
-								d.add(new JLabel("l'étudiant est déjà dans cette zone !"));
-								d.add(b);
-								d.setSize(300, 100);
-								d.setLocationRelativeTo(null);
-								d.setVisible(true);
+						JFrame f = new JFrame();
+						JDialog d;
+						d = new JDialog(f, "Erreur", true);
+						d.setLayout(new FlowLayout());
+						JButton b = new JButton("OK");
+						b.addActionListener(new ActionListener() {
+							public void actionPerformed(ActionEvent e) {
+								d.setVisible(false);
+							}
+						});
+						d.add(new JLabel("l'étudiant est déjà dans cette zone !"));
+						d.add(b);
+						d.setSize(300, 100);
+						d.setLocationRelativeTo(null);
+						d.setVisible(true);
 					} else {
 						System.out.println("OK");
 						System.out.println(survivantSelect.getNom());
@@ -229,7 +233,7 @@ public class TreveView {
 						survivantSelect.setStrategie(strat);
 						controller.deployerSurvivant(survivantSelect,
 								zoneDeploiementSurvivant.getSelectedIndex());
-						//update de la liste des survivants
+						// update de la liste des survivants
 						choixSurvivants.removeAll();
 						survivants = controller.getSurvivants();
 						for (Etudiant etu : survivants) {
@@ -243,7 +247,7 @@ public class TreveView {
 		});
 		treveView.add(deployerSurvivant);
 
-		//bouton valider 
+		// bouton valider
 
 		JButton btnValider = new JButton("Valider");
 		btnValider.setFont(new Font("Tahoma", Font.BOLD, 20));
@@ -256,21 +260,21 @@ public class TreveView {
 		});
 		affCreditZone();
 
-		//-------------------------------------------------------//
+		// -------------------------------------------------------//
 
-		//affichage profil etu selectionner
+		// affichage profil etu selectionner
 
-		//label 
+		// label
 		lblNbCredit = new JLabel("Credits : " + nbCreditEtuSelect);
 		lblNbCredit.setFont(new Font("Tahoma", Font.BOLD, 20));
 		lblNbCredit.setBounds(896, 350, 300, 26);
 		treveView.add(lblNbCredit);
-		//label 
+		// label
 		JLabel lnlStrategie = new JLabel("Stategie : ");
 		lnlStrategie.setFont(new Font("Tahoma", Font.BOLD, 20));
 		lnlStrategie.setBounds(896, 400, 300, 26);
 		treveView.add(lnlStrategie);
-		//choix de sa strategie
+		// choix de sa strategie
 		choixStrategie = new Choice();
 		choixStrategie.setFont(new Font("Tahoma", Font.BOLD, 20));
 		choixStrategie.setBounds(896, 450, 250, 30);
@@ -288,7 +292,7 @@ public class TreveView {
 	 * affiche les credits restants par zone
 	 */
 	private void affCreditZone() {
-		//affichage des credits restants par zone 
+		// affichage des credits restants par zone
 		JLabel lblCreditRestant = new JLabel("Credit Restant par zone");
 		lblCreditRestant.setFont(new Font("Tahoma", Font.BOLD, 20));
 		lblCreditRestant.setBounds(196, 460, 500, 26);
@@ -305,12 +309,13 @@ public class TreveView {
 					+ Utils.zoneIndexToString(i) + " = " + credit);
 			treveView.add(lblCreditZone[i]);
 		}
-		//affichage total credit
+		// affichage total credit
 		lblCreditTotal = new JLabel("Total = " + this.totalCredit);
 		lblCreditTotal.setFont(new Font("Tahoma", Font.BOLD, 20));
 		lblCreditTotal.setBounds(210, 650, 500, 26);
 		treveView.add(lblCreditTotal);
 	}
+
 	/**
 	 * met a jour la vue lorque l'utilisateur effectue une action
 	 */
@@ -329,16 +334,17 @@ public class TreveView {
 		else
 			reservisteSelect = null;
 
-
 		for (byte i = 0; i < 5; i++) {
 			int credit = controller.getCreditRestant(i);
 			this.totalCredit += credit;
 			lblCreditZone[i].setText(controleZoneString[controller.getControllee(i)] + "  "
 					+ Utils.zoneIndexToString(i) + " = " + credit);
 		}
-		//update nb credit 
+		// update nb credit
 		lblNbCredit.setText("Credits : " + nbCreditEtuSelect);
-		//update strategie
+		if (survivantSelect != null)
+			lblZoneActuel.setText("Zone : " + survivantSelect.getZone().getNomZone());
+		// update strategie
 		if (survivantSelect != null) {
 			if (survivantSelect.getStrategie() instanceof Defensif)
 				choixStrategie.select("Defensive");
